61f281d81704f3a2db7d7a5b78205e4400a1ade1
zaurkhalilli <ggencetel274@gmail.com>
2025-05-01 03:30:51 -0700

    lisenziyalarda duzelis olundu

--- fam-construction/src/Components/Lisenziyalar/Lisenziyalarmain.jsx
+++ fam-construction/src/Components/Lisenziyalar/Lisenziyalarmain.jsx
@@ -4,26 +4,47 @@ import Photo from '../Lisenziyalar/Lisenziya1.jpeg'
 import Photo2 from '../Lisenziyalar/Lisenziya2.jpeg'
 import Photo3 from '../Lisenziyalar/Lisenziya3.jpeg'
 import Photo4 from '../Lisenziyalar/Lisenziya4.jpeg'
+import Photo5 from '../Lisenziyalar/lisenziyacover.jpg'
 const Lisenziyalarmain = () => {
   return (
     <div id='lisenziyalar'>
       <div className="lisenziyalartop">
-     <img src="https://famgroupconstruction.az/templates/az/images/services-cat.jpg" alt="" />
+     <img src={Photo5} alt="FAM-Group-Construction-Lisenziyalar" />
      </div>
        <div className="lisenziyalaresas">
+        <h2>LİSENZİYALAR</h2>
+        <hr />
        <div className="container">
-            <div className="row g-4">
-                <div className="col-xl-6">
+            <div className="row g-5">
+                <div className="col-xl-12">
+                <div className="container">
+                  <div className="row g-3">
+                  <div className="col-xl-12">
+                    <p>1. 17 Fevral 2021 - ci ildə Azərbaycan Respublikası İqtisadiyyat Nazirliyi tərəfindən , tikintisinə icazə tələb olunan bina və qurğuların tikinti-quraşdırma işlərinin həyata keçirilməsinə icazə verilən lisenziya və lisenziya əlavəsi.</p>
+                    </div>
+                    <div className="col-xl-4">
+                    <img src={Photo3} alt="" />
+                    </div>
+                    <div className="col-xl-4">
+                    <img src={Photo2} alt="" />
+                    </div>
+                    <div className="col-xl-4">
+                    <img src={Photo} alt="" />
+                    </div>
+                  </div>
+                </div>
+                </div>
+                <div className="col-xl-12">
+                <div className="container">
+                  <div className="row g-3">
+                    <div className="col-xl-12">
+                    <p>15 Aprel 2021 - ci ildə Azərbaycan Respublikası İqtisadiyyat Nazirliyi tərəfindən , maye və təbii qaz təsərrüfatı obyektlərinin quraşdırılmasının həyata keçirilməsi haqqında icazə verilən lisenziya.</p>
+                    </div>
+                    <div className="col-xl-4">
                     <img src={Photo4} alt="" />
+                    </div>
+                  </div>
                 </div>
-                <div className="col-xl-6">
-                <img src={Photo2} alt="" />
-                </div>
-                <div className="col-xl-6">
-                <img src={Photo3} alt="" />
-                </div>
-                <div className="col-xl-6">
-                <img src={Photo} alt="" />
                 </div>
             </div>
         </div>
