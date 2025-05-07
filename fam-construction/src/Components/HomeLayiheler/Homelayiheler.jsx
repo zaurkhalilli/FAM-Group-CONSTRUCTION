ca5b9ef8df6a16c5889993af52a11596d112d832
zaurkhalilli <ggencetel274@gmail.com>
2025-05-07 01:12:27 -0700

    ana sehifeye elaveler olundu

--- fam-construction/src/Components/HomeLayiheler/Homelayiheler.jsx
+++ fam-construction/src/Components/HomeLayiheler/Homelayiheler.jsx
@@ -1,15 +1,26 @@
 import React from 'react'
 import '../HomeLayiheler/Homelayiheler.scss'
 import { Link } from 'react-router-dom';
+import Photonmk from '../NORM-MUHAFIZƏ-KÖŞKÜ/norm1.jpeg'
 const Homelayiheler = () => {
   return (
     <div id='layiheler'>
       <h3>BİTMİŞ VƏ DAVAM EDƏN LAYİHƏLƏR</h3>
        <div className="layiheimg">
        <div className="container">
-       <Link to="/layiheler" class="btn"> <img src="https://famgroupconstruction.az/uploads/posts/2023-07/1690657671_7.jpg" alt="" /> </Link>
+       <div className="row g-5">
+        <div className="col-xl-6">
+        <Link to="/layiheler" class="btn"> <img src="https://famgroupconstruction.az/uploads/posts/2023-07/1690657671_7.jpg" alt="" /> </Link>
        <div className="layihetext">
        <p>Gəncə şəhərində Bakcell Müştəri xidmətləri mərkəzinin təmiri</p>
+       </div>
+        </div>
+        <div className="col-xl-6">
+        <Link to="/layiheler" class="btn"> <img src="https://famgroupconstruction.az/uploads/posts/2023-07/1690654975_3.jpg" alt="" /> </Link>
+       <div className="layihetext">
+       <p>Fizuli rayonu Zobucuq 4 qəsəbəsindəki 50 metrəlik ötürücü baza stansiyasının inşası</p>
+       </div>
+        </div>
        </div>
        </div>
        </div>
